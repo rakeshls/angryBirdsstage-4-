a4d61a310e7f13d77b6a8501981818166adab963
rakeshls <rakeshls@rediffmail.com>
2020-11-27 18:46:48 +0530

    Catapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -17,10 +20,25 @@ class SlingShot{
 
     display(){
         if(this.sling.bodyA){
+            image(this.image1,200,60);
+            image(this.image2,170,60);
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8)
+            if(pointA.x<220){
+                strokeWeight(8);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+                }
+            else{
+                strokeWeight(3);
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y);
+            }
+                
+            image(this.image3,pointA.x-30,pointA.y-10,12,30);
+            
+            
         }
     }
     
